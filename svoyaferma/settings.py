3f42e0297203de502fcc895a4c631727d02219bd
d1mb0d <dimbod03@gmail.com>
2025-06-12 02:42:59 +0700

    fix2

--- svoyaferma/settings.py
+++ svoyaferma/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-dev-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-#ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+
 # Application definition
 
 INSTALLED_APPS = [
